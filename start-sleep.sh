9bec1a85963492539042c5e8daec9469a7ede9af
Adam Georgsson <adamgeorgsson@gmail.com>
2016-06-29 20:06:15 +0200

    cg-auto.sh phased out

---
+++ start-sleep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Code to stop
+/etc/init.d/networking stop
+echo 0 > /sys/devices/platform/soc/20980000.usb/buspower;
+#echo “Bus power stopping”
---
+++ stop-sleep.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Code to start
+echo 1 > /sys/devices/platform/soc/20980000.usb/buspower;
+#echo “Bus power starting”
+sleep 2;
+/etc/init.d/networking start
