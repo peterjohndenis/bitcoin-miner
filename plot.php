9475991fcdf9399983c92ef1c6ce436df25f0a6c
Ejdamm <adamgeorgsson@gmail.com>
2016-08-11 14:19:53 +0200

    changed paths

--- plot.php
+++ plot.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
 require 'phplot.php';
-$config = json_decode(file_get_contents('mysql_config.json'), true);
+$config = json_decode(file_get_contents('/var/www/mysql_config.json'), true);
 $server = $config['host'];
 $username = $config['login'];
 $password = $config['passwd'];
 $database = $config['database'];
 
-//$limit = isset($_SESSION['limit']) ? $_SESSION['limit'] : 100;
 $start = isset($_SESSION['start']) ? $_SESSION['start'] : (time() - (60*60*24*2));
 $end = isset($_SESSION['end']) ? $_SESSION['end'] : time();
 
@@ -27,15 +26,12 @@ if ($result->num_rows > 0)
 {
     //store data of each row
     while($row = $result->fetch_assoc()) 
-    {
-		//$data[] = array('', strtotime($row['time']), $row['voltage']);
 		$data[] = array('', $row['time'], $row['voltage']);
-    }
 } 
 
 
 $conn->close();
-$plot = new PHPlot(600, 400);
+$plot = new PHPlot(1200, 600);
 $plot->SetImageBorderType('plain');
 $plot->SetMarginsPixels(40, 40);
 $plot->SetDataValues($data);
