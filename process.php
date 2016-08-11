4068e66f43c6ab083469245f615a6a55cf6fc004
Adam <adamgeorgsson@gmail.com>
2016-08-11 15:00:38 +0200

    changed name to index.html

--- process.php
+++ process.php
@@ -4,4 +4,4 @@ if (!empty($_GET['start']))
 	$_SESSION['start'] = strtotime($_GET['start']);
 if (!empty($_GET['end']))
 	$_SESSION['end'] = strtotime($_GET['end']);
-header('Location: ./plot.html');
+header('Location: ./index.html');
