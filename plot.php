5d45de363b3f013a5ba50c7f6429461de59db956
Adam Georgsson <adamgeorgsson@gmail.com>
2016-09-10 16:29:28 +0200

    added temp plot

--- plot.php
+++ plot.php
@@ -21,30 +21,63 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM sensor_data WHERE time BETWEEN $start AND $end";
 $result = $conn->query($sql);
 
-$data = array();
+$volt_data = array();
+$temp_data = array();
 if ($result->num_rows > 0) 
 {
     //store data of each row
-    while($row = $result->fetch_assoc()) 
-		$data[] = array('', $row['time'], $row['voltage']);
-} 
-
-
+    while($row = $result->fetch_assoc())
+    { 
+		$volt_data[] = array('', $row['time'], $row['voltage'], 13.5, 11.7);
+		$temp_data[] = array('', $row['time'], $row['temp']);
+    }
+}
 $conn->close();
+ 
+$volt_legend = array('volt', 'upper limit', 'lower limit');
+$temp_legend = array('temp');
+
 $plot = new PHPlot(1200, 600);
-$plot->SetImageBorderType('plain');
-$plot->SetMarginsPixels(40, 40);
-$plot->SetDataValues($data);
+$plot->SetPrintImage(False);
+//$plot->SetImageBorderType('plain');
+
+
+//Volt plot
 $plot->SetPlotType('lines');
-$plot->SetLineWidths(2);
+$plot->SetLineStyles('solid');
 $plot->SetDataType('data-data');
+$plot->SetDataValues($volt_data);
 $plot->SetXLabelType('time', ' %d %b %H:%M ');
-$plot->SetPlotAreaWorld(NULL, 11.5, NULL, 14.7);
-$plot->SetYTickIncrement(0.1);
-//$plot->SetXTickIncrement(60*30);
-$plot->TuneYAutoRange(0, 'R', 0);
-$plot->SetFont('x_label', 2);
+$plot->SetXTickIncrement(60*120);
 $plot->SetXLabelAngle(90);
+$plot->SetFont('x_label', 2);
+$plot->SetLineWidths(2);
+$plot->SetYTitle("Volt");
+$plot->SetLegend($volt_legend);
+$plot->SetLegendPixels(0, 30);
+$plot->SetMarginsPixels(130, 110);
+$plot->SetPlotAreaWorld(NULL, 11.5, NULL, 14.5);
+$plot->SetDataColors(array('blue', 'green', 'orange'));
 $plot->DrawGraph();
+
+//Temp plot
+$plot->SetPlotType('lines');
+//$plot->SetDataType('data-data');
+$plot->SetDataValues($temp_data);
+$plot->SetLineWidths(2);
+$plot->SetYTitle("Temperature", 'plotright');
+$plot->SetLegend($temp_legend);
+$plot->SetLegendPixels(1120, 30);
+$plot->SetPlotAreaWorld(NULL, 20, NULL, 30);
+$plot->SetYTickIncrement(2);
+$plot->SetYTickPos('plotright');
+$plot->SetYTickLabelPos('plotright');
+$plot->SetDataColors('red');
+$plot->DrawGraph();
+
+
+
+$plot->PrintImage();
+
 
 session_unset(); 
